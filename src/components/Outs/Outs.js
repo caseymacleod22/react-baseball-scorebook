07e6cf0670e200cb1189f62f7a5bd0060bba1096
Casey MacLeod <caseymacleod22@gmail.com>
2021-05-24 11:54:08 -0400

    outs button now only goes up to 2, can't go below 0

--- src/components/Outs/Outs.js
+++ src/components/Outs/Outs.js
@@ -4,10 +4,18 @@ import {useState} from 'react'
     const [outs, setOuts] = useState(0)
 
     const increment = () => {
-        setOuts(outs + 1)
+        if(outs < 2) {
+            setOuts(outs + 1)
+        } else {
+            setOuts(0)
+        }
     }
     const decrement = () => {
-        setOuts(outs - 1)
+        if(outs > 1) {
+            setOuts(outs - 1)
+        } else {
+            setOuts(0)
+        }
     }
     const reset = () => {
         setOuts(0)
@@ -15,7 +23,7 @@ import {useState} from 'react'
 
     return (
         <div className='Outs'>
-            <h3>{outs}</h3>
+            <h3>Outs: {outs}</h3>
             <button class='button1' onClick={increment}>+1</button>
             <button class='button2' onClick={reset}>Reset Outs</button>
             <button class='button3' onClick={decrement}>-1</button>
