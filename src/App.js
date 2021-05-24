8c070f73abae658b690eb4327016d22f1d5aa9f6
Casey MacLeod <caseymacleod22@gmail.com>
2021-05-24 12:02:27 -0400

    adds balls component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header'
 import Outs from './components/Outs/Outs'
+import Balls from './components/Balls/Balls'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Outs />
+      <Balls />
     </div>
   );
 }
---
+++ src/components/Balls/Balls.js
@@ -0,0 +1,30 @@
+import {useState} from 'react'
+
+function Balls () {
+    const [balls, setBalls] = useState(0)
+
+    const increment = () => {
+        if(balls < 3) {
+            setBalls(balls + 1)
+        } else {
+            setBalls(0)
+        }
+    }
+    const decrement = () => {
+        if(balls > 1) {
+            setBalls(balls - 1)
+        } else {
+            setBalls(0)
+        }
+    }
+
+    return (
+        <div>
+            <h3>Balls: {balls}</h3>
+            <button className='button4' onClick={increment}>+1</button>
+            <button className='button5' onClick={decrement}>-1</button>
+        </div>
+    )
+}
+
+export default Balls
