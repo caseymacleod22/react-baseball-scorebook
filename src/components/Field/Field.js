b131e6913abdbaae3da9f7596534abc45510cf8e
Casey MacLeod <caseymacleod22@gmail.com>
2021-05-25 13:31:32 -0400

    wip:bases change color on click

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -1,30 +1,32 @@
+import { useState } from 'react'
 import '../../App.css'
 
 function Field () {
+    const [activeClass, setClass] = useState(false)
     return (
         <div id="stadium">
-    <div id="field" class="mowed-grass"></div>
+    <div id="field" className="mowed-grass"></div>
     <div id="in-field"></div>
-    <div id="in-field-grass" class="mowed-grass"></div>
+    <div id="in-field-grass" className="mowed-grass"></div>
     <div id="batting-circle"></div>
-    <div id="first-base" class="base">
-    </div>
-    <div id="second-base" class="base">
-    </div>
-    <div id="thrid-base" class="base">
-    </div>
     <div id="home-plate">
-      <div class="action-area"></div>
+      <div className="action-area"></div>
     </div>
-    <div class="home-plate-pulse pulse"></div>
+    <button onClick={() => activeClass === true ? setClass(false) : setClass(true)} style={{"background-color": activeClass ? "yellow" : "white"}} id="first-base">
+    </button>
+    <button onClick={() => activeClass === true ? setClass(false) : setClass(true)} style={{"background-color": activeClass ? "yellow" : "white"}} id="second-base">
+    </button>
+    <button onClick={() => activeClass === true ? setClass(false) : setClass(true)} style={{"background-color": activeClass ? "yellow" : "white"}} id="third-base">
+    </button>
+    <div className="home-plate-pulse pulse"></div>
     <div id="base-lines"></div>
     <div id="pitchers-mound"></div>
     <div id="pitchers-plate"></div>
-    <div id="first-circle" class="half-circle"></div>
-    <div id="second-circle" class="half-circle"></div>
-    <div id="third-circle" class="half-circle"></div>
-    <div id="batters-box-right" class="batters-box"></div>
-    <div id="batters-box-left" class="batters-box"></div>
+    <div id="first-circle" className="half-circle"></div>
+    <div id="second-circle" className="half-circle"></div>
+    <div id="third-circle" className="half-circle"></div>
+    <div id="batters-box-right" className="batters-box"></div>
+    <div id="batters-box-left" className="batters-box"></div>
     <div id="first-base-thing"></div>
 </div>
     )
